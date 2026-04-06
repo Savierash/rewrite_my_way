8a4563019158984b61c31b18352f1f59cbc51357
Kylo <dump1473@gmail.com>
2026-04-06 14:22:42 +0800

    delete netlify

--- rewrite-my-way/vite.config.js
+++ rewrite-my-way/vite.config.js
@@ -3,4 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/rewrite_my_way/', // This must match your GitHub repository name exactly
 })
